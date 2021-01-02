190de6002180e8397a655e180e191ad8c20692be
gracevh <66805585+gracevh@users.noreply.github.com>
2021-01-02 02:22:42 -0500

    Create Decision Trees.R

---
+++ Decision Trees.R
@@ -0,0 +1,136 @@
+##############################################
+##  Decision Trees for Variable Importance  ##
+##############################################
+library(tree)
+library(magrittr)
+
+## Portuguese ##
+set.seed(10)
+p.tree = tree(G3 ~ ., data = Por.val$train)
+plot(p.tree) # tree plot
+text(p.tree) # text corresponding to tree
+
+pred.y = predict(p.tree, newdata = Por.val$val)
+mean((pred.y - unlist(Por.val$val.y))^2) # must unlist the y responses
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) # put in best to prune the tree
+plot(prune.ptree)
+text(prune.ptree, pretty = 0)
+
+prune.y = predict(prune.ptree, newdata = Por.val$val)
+mean((prune.y - unlist(Por.val$val.y))^2) # must unlist the y responses
+
+# Final test on pruned tree
+pred.y = predict(prune.ptree, newdata = Por.val$test)
+mean((pred.y - unlist(Por.val$test.y))^2)
+
+# For categorical testing
+# Binary Pass/Fail
+set.seed(10)
+p.tree = tree(G3 ~ ., data=por.trn[,-c(34,32)])
+plot(p.tree)
+text(p.tree)
+
+pred.y = predict(p.tree, newdata = Por.val$val, type = "class")
+1 - mean(pred.y == Por.val$val.y) 
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) 
+prune.y = predict(prune.ptree, newdata = Por.val$val, type = "class")
+1 - mean(prune.y == Por.val$val.y) 
+
+pred.y = predict(prune.cptree, newdata = Por.val$test, type = "class")
+1 - mean(pred.y == Por.val$test.y)
+
+# Competitive comp/noncomp
+set.seed(10)
+p.tree = tree(G3 ~ ., data=por.trn[,-c(34,32)])
+plot(p.tree)
+text(p.tree)
+
+pred.y = predict(p.tree, newdata = Por.val$val, type = "class")
+1 - mean(pred.y == Por.val$val.y) 
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) 
+prune.y = predict(prune.ptree, newdata = Por.val$val, type = "class")
+1 - mean(prune.y == Por.val$val.y) 
+
+pred.y = predict(prune.cptree, newdata = Por.val$test, type = "class")
+1 - mean(pred.y == Por.val$test.y)
+
+
+## Math ##
+set.seed(10)
+p.tree = tree(G3 ~ ., data = Por.val$train)
+plot(p.tree) # tree plot
+text(p.tree) # text corresponding to tree
+
+pred.y = predict(p.tree, newdata = Por.val$val)
+mean((pred.y - unlist(Por.val$val.y))^2) # must unlist the y responses
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) # put in best to prune the tree
+plot(prune.ptree)
+text(prune.ptree, pretty = 0)
+
+prune.y = predict(prune.ptree, newdata = Por.val$val)
+mean((prune.y - unlist(Por.val$val.y))^2) # must unlist the y responses
+
+# Final test on pruned tree
+pred.y = predict(prune.ptree, newdata = Por.val$test)
+mean((pred.y - unlist(Por.val$test.y))^2)
+
+# For categorical testing
+# Binary Pass/Fail
+set.seed(10)
+p.tree = tree(G3 ~ ., data=por.trn[,-c(34,32)])
+plot(p.tree)
+text(p.tree)
+
+pred.y = predict(p.tree, newdata = Por.val$val, type = "class")
+1 - mean(pred.y == Por.val$val.y) 
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) 
+prune.y = predict(prune.ptree, newdata = Por.val$val, type = "class")
+1 - mean(prune.y == Por.val$val.y) 
+
+pred.y = predict(prune.cptree, newdata = Por.val$test, type = "class")
+1 - mean(pred.y == Por.val$test.y)
+
+# Competitive comp/noncomp
+set.seed(10)
+p.tree = tree(G3 ~ ., data=por.trn[,-c(34,32)])
+plot(p.tree)
+text(p.tree)
+
+pred.y = predict(p.tree, newdata = Por.val$val, type = "class")
+1 - mean(pred.y == Por.val$val.y) 
+
+p.tree %>% 
+  cv.tree(.) %$% 
+  size[which.min(dev)] # index the deviance for the best size of tree based on minimum deviance score
+
+prune.ptree = prune.tree(p.tree, best = 5) 
+prune.y = predict(prune.ptree, newdata = Por.val$val, type = "class")
+1 - mean(prune.y == Por.val$val.y) 
+
+pred.y = predict(prune.cptree, newdata = Por.val$test, type = "class")
+1 - mean(pred.y == Por.val$test.y)
